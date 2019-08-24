4a1bfd278801b47b98729302ce457daa583ddd00
yangdinghui <1004329447@qq.com>
2019-08-24 21:58:02 +0800

    项目初始化

--- src/main/java/com/jscloud/login/controller/LoginController.java
+++ src/main/java/com/jscloud/login/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.jscloud.login;
+package com.jscloud.login.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
